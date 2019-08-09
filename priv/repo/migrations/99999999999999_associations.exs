36c2f8004042116558d41f2dcc3686527d82edb3
Jason Clark <mithereal@gmail.com>
2019-08-09 12:55:27 -0700

    .

--- priv/repo/migrations/99999999999999_associations.exs
+++ priv/repo/migrations/99999999999999_associations.exs
@@ -4,7 +4,7 @@ defmodule DB.Repo.Migrations.License.Associations do
   def change do
     alter table(:licenses) do
       add(:license_id, references(:license, on_delete: :nothing))
-      add(:user_id, references(:license_user, on_delete: :nothing))
+      add(:user_id, references(:license_users, on_delete: :nothing))
     end
   end
 end
