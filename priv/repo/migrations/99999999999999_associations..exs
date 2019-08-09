6d45929b83f90f02f61365d733ec8f9112cb1b37
Jason Clark <mithereal@gmail.com>
2019-08-09 12:37:17 -0700

    fix migration

--- priv/repo/migrations/99999999999999_associations..exs
+++ priv/repo/migrations/99999999999999_associations..exs
@@ -4,8 +4,8 @@ defmodule DB.Repo.Migrations.License.User do
   def change do
 
     alter table(:licenses) do
-      add : license_id, references(:license, on_delete: :nothing)
-      add : user_id, references(:license_user, on_delete: :nothing)
+      add(:license_id, references(:license, on_delete: :nothing))
+      add(:user_id, references(:license_user, on_delete: :nothing))
     end
 
 end
